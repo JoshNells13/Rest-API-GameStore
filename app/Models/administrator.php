dee2c6f2b0c9c692c97621f484d50043ac21e056
JoshNells13 <joshua.plk08@gmail.com>
2026-02-18 17:59:20 +0700

    Refactor administrator model and authentication configuration: change administrator class to extend Authenticatable, add last_login_at to fillable fields, and configure admin guard and administrators provider in auth and sanctum settings.

--- app/Models/administrator.php
+++ app/Models/administrator.php
@@ -3,12 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class administrator extends Model
+class administrator extends Authenticatable
 {
-     use HasApiTokens,HasFactory;
+     use HasApiTokens, HasFactory, Notifiable;
 
-     protected $fillable = ['username','password'];
+     protected $fillable = ['username', 'password', 'last_login_at'];
+
+     protected $hidden = [
+         'password',
+         'remember_token',
+     ];
+
+     protected function casts(): array
+     {
+         return [
+             'password' => 'hashed',
+         ];
+     }
 }
